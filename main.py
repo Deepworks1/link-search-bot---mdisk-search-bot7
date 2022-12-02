330fe606a972924d46ef95cc8f7c4dca27617917
Deepworks1 <119611140+Deepworks1@users.noreply.github.com>
2022-12-02 18:38:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ User = Client(
 
 @Bot.on_message(filters.private & filters.command("start"))
 async def start_handler(_, event: Message):
-	await event.reply_photo("https://telegra.ph/file/19eeb26fa2ce58765917a.jpg",
+	await event.reply_photo("https://telegra.ph/file/35857d4d81a27bf20bdeb.jpg",
                                 caption=Config.START_MSG.format(event.from_user.mention),
                                 reply_markup=InlineKeyboardMarkup([
 					[InlineKeyboardButton('Our Group', url='https://t.me/+x_SyirDsEUs4YmJl')],
